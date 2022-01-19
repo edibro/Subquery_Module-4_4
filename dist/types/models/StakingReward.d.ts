027b0b9f16875b18aabd442b290e159c117b0115
edibro <edward.brovik@gmail.com>
2022-01-20 02:13:01 +0300

    module 4_3

--- dist/types/models/StakingReward.d.ts
+++ dist/types/models/StakingReward.d.ts
@@ -2,12 +2,13 @@ import { Entity, FunctionPropertyNames } from "@subql/types";
 export declare class StakingReward implements Entity {
     constructor(id: string);
     id: string;
-    account: string;
+    accountId: string;
     balance: bigint;
     date: Date;
-    blockHeight: number;
+    blockheight?: number;
     save(): Promise<void>;
     static remove(id: string): Promise<void>;
     static get(id: string): Promise<StakingReward | undefined>;
+    static getByAccountId(accountId: string): Promise<StakingReward[] | undefined>;
     static create(record: Partial<Omit<StakingReward, FunctionPropertyNames<StakingReward>>> & Entity): StakingReward;
 }
